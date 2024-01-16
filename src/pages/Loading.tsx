58c819c906fd134779599e575819f5a121914a68
m-maaz-azhar <maazazher28@gmail.com>
2024-01-16 16:30:58 +0500

    Removed unused React import

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function Loading() {
     return (
         <div className="bg-gradient-to-r from-blue-500 via-purple-600 to-pink-500 min-h-screen p-8 text-white">
